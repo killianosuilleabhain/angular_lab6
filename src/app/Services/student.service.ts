9238eee19d4e6aeb31308b14409cf47f2a48aca5
killianosuilleabhain <killianosuilleabhain>
2021-03-09 19:41:57 +0000

    lab upload

---
+++ src/app/Services/student.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  constructor(private http:HttpClient) { }
+
+  GetStudentData():Observable<any>{
+    return this.http.get('https://www.jsonblob.com/api/jsonblob/b9264545-8103-11eb-82da-7fe921df73a6');
+  }
+
+  GetWeatherData():Observable<any>{
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Galway&APPID=20d8726305b1bfb3ea64b0badb2e916a');
+  }
+}
